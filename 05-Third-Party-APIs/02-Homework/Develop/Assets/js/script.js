e6e8cbf8e61faebe983e5229dc79697e9473cc13
Tibegex <joshua_b_lee@yahoo.com>
2021-04-29 15:00:32 -0600

    added in the js script file and started making my time and day dispaly

---
+++ 05-Third-Party-APIs/02-Homework/Develop/Assets/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  //write all of our jquery inside this function
+  //variable declarations
+
+  //Display the date to the user
+  //first, gra  reference to the element that we wnat the time to be displayed
+  var currentDay = $("#currentDay");
+  //dispaly the current date in the p element
+  currentDaty.text(moment().format("dddd MMM Do YYYY"));
+
+  //functions
+
+  //event handlers
+});
